cda7283658053d0872c3f6189a385d6c4c56ebe7
Emma <xjoelee@gmail.com>
2026-09-12 13:18:03 +0900

    emma:260912-1318

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    basePath: '/sfumonai', // 모든 라우팅과 _next 정적 자산의 기준 경로 설정
 };
 
 export default nextConfig;
